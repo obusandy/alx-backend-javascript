1d1ab8c89574041c6942c405e8bb605cd54d86e7
obusandy <obungasandrah@gmail.com>
2024-05-27 22:49:17 +0000

    my changes

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,3 @@
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
+} //  function to 1 line - without changing the name of each function/variable.
