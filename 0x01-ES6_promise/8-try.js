57a840c8e5faa02bad7b5a6e27e8560aed931044
obusandy <obungasandrah@gmail.com>
2024-05-30 12:24:55 +0000

    try catch

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,8 @@
+/* accepts two arguments: numerator (Number) and denominator (Number). */
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  } else {
+    return numerator / denominator;
+  }
+}
