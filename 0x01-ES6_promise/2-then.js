d35d13048b59d7e06adb94700a6ab54bf59337ed
obusandy <obungasandrah@gmail.com>
2024-05-30 12:03:06 +0000

    Catch me if you can!

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+/* When the Promise resolves, return an object */
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
